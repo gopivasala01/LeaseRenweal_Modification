7d29c874b56aa8c9ddf54b55a003df19cf57d43e
gopivasala01 <gopi.v@beetlerim.com>
2023-08-01 02:45:23 -0400

    Admin fee issue fix in NC and SC

--- src/main/java/PDFDataExtract/SouthCarolina_Format2.java
+++ src/main/java/PDFDataExtract/SouthCarolina_Format2.java
@@ -143,7 +143,11 @@ public class SouthCarolina_Format2
 	    	PDFReader.adminFee = text.substring(text.indexOf(PDFAppConfig.SouthCarolina_Format2.adminFee_prior)+PDFAppConfig.SouthCarolina_Format2.adminFee_prior.length()).split(" ")[0].trim();
 	    	if(PDFReader.adminFee.matches(".*[a-zA-Z]+.*"))
 		    {
-		    	PDFReader.adminFee = "Error";
+	    		PDFReader.adminFee = text.substring(text.indexOf(PDFAppConfig.SouthCarolina_Format2.adminFee_prior2)+PDFAppConfig.SouthCarolina_Format2.adminFee_prior2.length()).split(" ")[0].trim();
+	    		if(PDFReader.adminFee.matches(".*[a-zA-Z]+.*"))
+			    {
+			    	PDFReader.adminFee = "Error";
+			    }
 		    }
 	    }
 	    catch(Exception e)
